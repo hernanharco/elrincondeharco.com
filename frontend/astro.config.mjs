40c8335231a39243c6b6aec24e4a223d7c848569
Hernan Arango <hernan.harco@gmail.com>
2026-05-07 07:17:07 +0200

    v1.3.0 "actualizacion de informacion de prototipos"

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -4,4 +4,9 @@ import svelte from '@astrojs/svelte';
 export default defineConfig({
   // Solo dejamos Svelte, Tailwind se encargará mediante PostCSS/Vite
   integrations: [svelte()],
+  server: {
+    port: 4322,
+    strictPort: true, // Evita que Astro salte a otro puerto si el 4322 está ocupado
+    host: true       // Útil si necesitas acceder desde otros dispositivos en tu red local
+  }
 });
